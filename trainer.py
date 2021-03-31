bd6c587a73b0d1b6b0126215e205c74c5303b35a
tyler <49506255+tyistyler@users.noreply.github.com>
2021-03-31 08:05:02 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -120,7 +120,7 @@ class Trainer(object):
                     break
                     
             res = self.evaluate("dev")  # fine-tuning
-            if self.best_f < res['f_score']:
+            if self.best_f < res['slot_f1']:
                 self.save_model()
             
             if 0 < self.args.max_steps < global_step:
